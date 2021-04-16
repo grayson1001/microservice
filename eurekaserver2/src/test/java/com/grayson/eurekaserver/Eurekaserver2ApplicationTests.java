e0e1a3c042114ec7e0f20cb315e81d4f55aea730
grayson <zhanglong04@baidu.com>
2021-04-16 15:34:07 +0800

    create project

---
+++ eurekaserver2/src/test/java/com/grayson/eurekaserver/Eurekaserver2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grayson.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eurekaserver2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
